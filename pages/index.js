cf16da0267106a7a2a0d2eee906eabed75794a6a
Diego Lisboa Giglioli <d.lisboa.giglioli@avanade.com>
2023-05-30 19:41:01 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export function getStaticProps() {
+  return {
+    props: {
+      time: new Date().toLocaleTimeString(),
+    }
+  };
+}
+
+export default function Home({ time }) {
+  function revalidate() {
+    fetch("/api/revalidate");
+  }
+  return (
+    <div>
+      <h1>{time}</h1>
+      <button onClick={() => revalidate()}>Revalidar</button>
+    </div>
+  );
+}
